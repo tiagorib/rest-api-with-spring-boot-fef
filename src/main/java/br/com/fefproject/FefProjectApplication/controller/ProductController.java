b2cdcb7aab7f3e545216ee8c99f89009959315af
Tiago Ribeiro Carneiro <tiago.rib@gmail.com>
2023-03-22 22:33:28 -0300

    feat: implements delete, find and update methods for Products

--- src/main/java/br/com/fefproject/FefProjectApplication/controller/ProductController.java
+++ src/main/java/br/com/fefproject/FefProjectApplication/controller/ProductController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/fef/v1/product")
@@ -27,6 +29,24 @@ public class ProductController {
     @PostMapping(value = "/insert")
     public ResponseEntity<Object> saveProduct(@RequestBody Product product) {
         Product result = productService.saveProduct(product);
+        return ResponseEntity.ok().body(ResponseGenericException.response(result));
+    }
+
+    @DeleteMapping(value = "/delete/{idProduct}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable Long idProduct) {
+        HashMap<String, Object> result = productService.deleteProduct(idProduct);
+        return ResponseEntity.ok().body(ResponseGenericException.response(result));
+    }
+
+    @GetMapping(value = "/findProduct/{idProduct}")
+    public ResponseEntity<Object> findProduct(@PathVariable Long idProduct) {
+        Optional<Product> result = productService.findProductById(idProduct);
+        return ResponseEntity.ok().body(ResponseGenericException.response(result));
+    }
+
+    @PutMapping(value = "/update")
+    public ResponseEntity<Object> updateProduct(@RequestBody Product product) {
+        Product result = productService.updateProduct(product);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
 
--- src/main/java/br/com/fefproject/FefProjectApplication/service/ProductService.java
+++ src/main/java/br/com/fefproject/FefProjectApplication/service/ProductService.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -21,9 +23,7 @@ public class ProductService {
     }
 
     public Product saveProduct(Product product) {
-
-        if (product.getAmountProduct() != null &&
-                product.getAmountProduct().compareTo(BigDecimal.valueOf(0)) == 1) {
+        if (validateProduct(product)) {
             return productRepository.saveAndFlush(product);
         } else {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
@@ -31,5 +31,50 @@ public class ProductService {
                             "obrigatório e deve ser maior que 0 (zero)!");
         }
     }
+
+    public HashMap<String, Object> deleteProduct(Long productId) {
+        Optional<Product> product =
+                Optional.ofNullable(productRepository.findById(productId).
+                        orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                "Produto não encontrado!")));
+
+
+        productRepository.delete(product.get());
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        result.put("result", "Produto: " + product.get().getNameProduct() + " excluído com sucesso!");
+        return result;
+    }
+
+    public Optional<Product> findProductById(Long idProduct) {
+        return Optional.ofNullable(productRepository.findById(idProduct)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Produto não encontrado!")));
+    }
+
+    public Product updateProduct(Product product) {
+        if (validateProduct(product)) {
+            if (findProductById(product.getIdProduct()) != null) {
+                return productRepository.saveAndFlush(product);
+            } else {
+                return null;
+            }
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "O preço de custo e preço de venda do produto são obrigatórios " +
+                            "e devem ser maiores que 0 (zero)!");
+        }
+    }
+
+    public Boolean validateProduct(Product product) {
+        if (product.getAmountProduct() != null &&
+                product.getAmountProduct().compareTo(BigDecimal.valueOf(0)) == 1 &&
+                product.getCostPriceProduct() != null &&
+                product.getCostPriceProduct().compareTo(BigDecimal.valueOf(0)) == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
 }
