cd6390694f50a1547fb1effac46a4f1484cc7f83
caahhcarvalho <ana.carvalhosi2021@gmail.com>
2023-03-29 21:35:01 -0300

    feat: implement customer crud

---
+++ src/main/java/br/com/fefproject/FefProjectApplication/controller/CustomerController.java
@@ -0,0 +1,51 @@
+package br.com.fefproject.FefProjectApplication.controller;
+import br.com.fefproject.FefProjectApplication.entity.Customer;
+import br.com.fefproject.FefProjectApplication.entity.Product;
+import br.com.fefproject.FefProjectApplication.exception.ResponseGenericException;
+import br.com.fefproject.FefProjectApplication.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/fef/v1/customer")
+@CrossOrigin(value = "*")
+
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping(value = "/list")
+    public ResponseEntity<Object> getInfoCustomers(){
+        List<Customer> result = customerService.getInfoCustomers();
+        return ResponseEntity.ok().body(ResponseGenericException.response(result));
+    }
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<Object> saveCustomer(@RequestBody Customer customer) {
+        Customer result = customerService.saveCustomer(customer);
+        return ResponseEntity.ok().body(ResponseGenericException.response(result));
+    }
+
+    @DeleteMapping(value = "/delete/{idCustomer}")
+    public ResponseEntity<Object> deleteCustomer(@PathVariable Long idCustomer) {
+        HashMap<String, Object> result = customerService.deleteCustomer(idCustomer);
+        return ResponseEntity.ok().body(ResponseGenericException.response(result));
+    }
+
+    @GetMapping(value = "/findCustomer/{idCustomer}")
+    public ResponseEntity<Object> getCustomerById(@PathVariable Long idCustomer){
+        Customer result = customerService.findCustomerById(idCustomer);
+        return ResponseEntity.ok().body(ResponseGenericException.response(result));
+    }
+
+    @PutMapping(value = "/update")
+    public ResponseEntity<Object> updateCustomer(@RequestBody Customer customer) {
+        Customer result = customerService.updateCustomer(customer);
+        return ResponseEntity.ok().body(ResponseGenericException.response(result));
+    }
+
+}
--- src/main/java/br/com/fefproject/FefProjectApplication/entity/Customer.java
+++ src/main/java/br/com/fefproject/FefProjectApplication/entity/Customer.java
@@ -1,19 +1,58 @@
 package br.com.fefproject.FefProjectApplication.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_customer")
     private Long idCustomer;
+
+    @Column(name = "first_name_customer", unique = true, nullable = false, length = 300)
     private String firstNameCustomer;
+
+    @Column(name = "last_name_customer", unique = true, nullable = false, length = 300)
     private String lastNameCustomer;
+
+    @Column(name = "cpf_customer", unique = true, nullable = false, length = 14)
     private String cpfCustomer;
-    private LocalDate birthdateCustomer;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @Column(name = "birthDate_Customer", nullable = false)
+    private LocalDate birthDateCustomer;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @Column(name = "dateCreated_Customer", nullable = false, updatable = false )
     private LocalDate dateCreatedCustomer;
+
+    @Column(name = "monthlyIncome_Customer", nullable = false, precision = 10, scale = 2)
     private BigDecimal monthlyIncomeCustomer;
+
+    @Column(name = "status_Customer", nullable = false)
     private Boolean statusCustomer;
+
+    @Column(name = "email_Customer", unique = true, nullable = false, length = 300)
     private String emailCustomer;
+
+    @Column(name = "password_Customer")
     private String passwordCustomer;
+
+    @PrePersist
+    public void prePersist(){
+        setDateCreatedCustomer(LocalDate.now());
+    }
+
 
 }
