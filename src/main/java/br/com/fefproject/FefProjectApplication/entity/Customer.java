3b19785ace86b0f34b221190190929bd124b6621
Admin <joao.lucas280398@gmail.com>
2023-04-06 00:30:54 -0300

    joao

--- src/main/java/br/com/fefproject/FefProjectApplication/entity/Customer.java
+++ src/main/java/br/com/fefproject/FefProjectApplication/entity/Customer.java
@@ -1,19 +1,57 @@
 package br.com.fefproject.FefProjectApplication.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.validator.constraints.br.CPF;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+import javax.validation.constraints.Email;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idCustomer")
     private Long idCustomer;
-    private String firstNameCustomer;
-    private String lastNameCustomer;
-    private String cpfCustomer;
-    private LocalDate birthdateCustomer;
-    private LocalDate dateCreatedCustomer;
-    private BigDecimal monthlyIncomeCustomer;
-    private Boolean statusCustomer;
-    private String emailCustomer;
-    private String passwordCustomer;
 
+    @Column(name = "firstNameCustomer", unique = true, nullable = false, length = 300)
+    private String firstNameCustomer;
+    @Column(name = "lastNameCustomer", unique = true, nullable = false, length = 300)
+    private String lastNameCustomer;
+
+    @CPF
+    @Column(name = "cpfCustomer", unique = true, nullable = false, length = 300)
+    private String cpfCustomer;
+
+    @Column
+    private LocalDate birthdateCustomer;
+
+    @Column
+    private LocalDate dateCreatedCustomer;
+
+    @Column(name = "monthlyIncomeCustomer", nullable = false, precision = 10, scale = 2)
+    private BigDecimal monthlyIncomeCustomer;
+
+    @Column(name = "statusCustomer", nullable = false)
+    private Boolean statusCustomer;
+
+    @Email
+    @Column(name = "emailCustomer", unique = true, nullable = false, length = 300)
+    private String emailCustomer;
+
+    @Column(name = "passwordCustomer", unique = true, nullable = false, length = 300)
+        private String passwordCustomer;
 }
--- src/main/java/br/com/fefproject/FefProjectApplication/repository/CustomerRepository.java
+++ src/main/java/br/com/fefproject/FefProjectApplication/repository/CustomerRepository.java
@@ -3,5 +3,5 @@ package br.com.fefproject.FefProjectApplication.repository;
 import br.com.fefproject.FefProjectApplication.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustumerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
