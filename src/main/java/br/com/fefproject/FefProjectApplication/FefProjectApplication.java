1f986617407036c55ae28ae0076c9309aa0aeb5b
Admin <joao.lucas280398@gmail.com>
2023-04-12 22:15:46 -0300

    JoaoL

--- src/main/java/br/com/fefproject/FefProjectApplication/FefProjectApplication.java
+++ src/main/java/br/com/fefproject/FefProjectApplication/FefProjectApplication.java
@@ -1,9 +1,15 @@
 package br.com.fefproject.FefProjectApplication;
 
+
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Rest API FEF", description = "REST API FEF SpringBoot") )
+
 public class FefProjectApplication {
 
 	public static void main(String[] args) {
