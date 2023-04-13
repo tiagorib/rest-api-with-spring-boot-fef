04891c5b7ed497791c053246f5e1ae3c9cd5901b
Igor Buosi <igorbiancobuosi@hotmail.com>
2023-04-12 21:47:42 -0300

    igor

--- src/main/java/br/com/fefproject/FefProjectApplication/FefProjectApplication.java
+++ src/main/java/br/com/fefproject/FefProjectApplication/FefProjectApplication.java
@@ -1,10 +1,16 @@
 package br.com.fefproject.FefProjectApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-public class FefProjectApplication {
+@OpenAPIDefinition(info = @Info(title = "Rest API FEF", description = "REST API FEF SpringBoot"))
+public class FefProjectApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(FefProjectApplication.class, args);
