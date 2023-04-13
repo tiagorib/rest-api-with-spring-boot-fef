5575b4e157866be96468fe8910d8b23cb59a8f6f
RhuanTodra <rhuancrystyan@gmail.com>
2023-04-12 21:58:54 -0300

    feat: Swagger

--- src/main/java/br/com/fefproject/FefProjectApplication/controller/CustomerController.java
+++ src/main/java/br/com/fefproject/FefProjectApplication/controller/CustomerController.java
@@ -3,6 +3,7 @@ package br.com.fefproject.FefProjectApplication.controller;
 import br.com.fefproject.FefProjectApplication.entity.Customer;
 import br.com.fefproject.FefProjectApplication.exception.ResponseGenericException;
 import br.com.fefproject.FefProjectApplication.service.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,30 +21,35 @@ public class CustomerController {
     private CustomerService customerService;
 
     @GetMapping(value = "/list")
+    @Operation(summary = "List all the customers")
     public ResponseEntity<Object> getInfoCustomers() {
         List<Customer> result = customerService.getInfoCustomers();
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
 
     @PostMapping(value = "/insert")
+    @Operation(summary = "Method for insert customers in database")
     public ResponseEntity<Object> saveCustomer(@RequestBody Customer customer) {
         Customer result = customerService.saveCustomer(customer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
 
     @DeleteMapping(value = "/delete/{idCustomer}")
+    @Operation(summary = "Method for delete customers from database")
     public ResponseEntity<Object> deleteCustomer(@PathVariable Long idCustomer) {
         HashMap<String, Object> result = customerService.deleteCustomer(idCustomer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
 
     @GetMapping(value = "/findCustomer/{idCustomer}")
+    @Operation(summary = "Method for find customers by idCustomer in database")
     public ResponseEntity<Object> findCustomer(@PathVariable Long idCustomer) {
         Optional<Customer> result = customerService.findCustomerById(idCustomer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
 
     @PutMapping(value = "/update")
+    @Operation(summary = "Method for update customers in database")
     public ResponseEntity<Object> updateCustomer(@RequestBody Customer Customer) {
         Customer result = customerService.updateCustomer(Customer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
--- src/main/java/br/com/fefproject/FefProjectApplication/controller/ProductController.java
+++ src/main/java/br/com/fefproject/FefProjectApplication/controller/ProductController.java
@@ -4,6 +4,7 @@ import br.com.fefproject.FefProjectApplication.entity.Product;
 import br.com.fefproject.FefProjectApplication.exception.ResponseGenericException;
 import br.com.fefproject.FefProjectApplication.repository.ProductRepository;
 import br.com.fefproject.FefProjectApplication.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,30 +22,35 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping(value = "/list")
+    @Operation(summary = "Method for list all products from database")
     public ResponseEntity<Object> getInfoProducts() {
         List<Product> result = productService.getInfoProducts();
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
 
     @PostMapping(value = "/insert")
+    @Operation(summary = "Method for save products in database")
     public ResponseEntity<Object> saveProduct(@RequestBody Product product) {
         Product result = productService.saveProduct(product);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
 
     @DeleteMapping(value = "/delete/{idProduct}")
+    @Operation(summary = "Method for delete product from database")
     public ResponseEntity<Object> deleteProduct(@PathVariable Long idProduct) {
         HashMap<String, Object> result = productService.deleteProduct(idProduct);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
 
     @GetMapping(value = "/findProduct/{idProduct}")
+    @Operation(summary = "Method for find products by idProduct in database")
     public ResponseEntity<Object> findProduct(@PathVariable Long idProduct) {
         Optional<Product> result = productService.findProductById(idProduct);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
 
     @PutMapping(value = "/update")
+    @Operation(summary = "Method for update customers in database")
     public ResponseEntity<Object> updateProduct(@RequestBody Product product) {
         Product result = productService.updateProduct(product);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
